20d135b7ccd531f716c79b52a7206238dd3dab12
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-11-20 14:25:37 +0630

    added remote host to client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://freedom-codes-xde8.vercel.app";
 export default axios.create({
   baseURL: BASE_URL,
 });
