07d835216573d848caf41712494285ce9e14c97a
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-11-20 17:33:40 +0630

    --

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://freedom-codes-xde8.vercel.app";
+const BASE_URL = "http://localhost:3500";
 export default axios.create({
   baseURL: BASE_URL,
 });
