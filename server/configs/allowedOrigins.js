8412128163985e1fc73bab650f8f645e7a617fda
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-12-10 11:38:21 +0630

    vercel domain allowed in cors

--- server/configs/allowedOrigins.js
+++ server/configs/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://freedom-codes.onrender.com",
+  "https://freedom-codes.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
