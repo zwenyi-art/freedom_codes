32046d189729839c8c249aa7ef2f1bbd4c2527cc
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2024-12-13 13:22:26 +0630

    landing page add navbar and some welcome content

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,6 +4,9 @@ import useAuth from "../hooks/useAuth";
 import { IoMdLogIn } from "react-icons/io";
 import { title } from "framer-motion/client";
 import { HiMiniLanguage } from "react-icons/hi2";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaLanguage } from "react-icons/fa6";
 const welcome_content = {
   en: {
     title: "Welcome To Freedom Codes For Myanmar",
@@ -30,6 +33,7 @@ const LandingPage = () => {
   const [background, setBackground] = useState("");
   const [language, setLanguage] = useState("en");
   const [languageBox, setLanguageBox] = useState(false);
+  const [modalBox, setModalBox] = useState(false);
   console.log("landing page", auth);
 
   useEffect(() => {
@@ -57,11 +61,17 @@ const LandingPage = () => {
       setLanguage("en");
     }
   };
+  useEffect(() => {
+    console.log(modalBox);
+  }, [modalBox]);
   return (
-    <main className="w-full h-svh flex flex-col gap-y-3 text-white mx-auto container">
-      <nav className="w-full h-fit  flex flex-row items-center justify-between py-1">
+    <main
+      onClick={() => modalBox && setModalBox(false)}
+      className="relative w-full h-svh flex flex-col gap-y-3 text-white mx-auto container"
+    >
+      <nav className="w-full h-fit bg-black/30 sm:bg-transparent flex flex-row items-center justify-between px-3  py-4 sm:px-0 sm:py-1">
         <h1>FREEDM</h1>
-        <div className="flex-auto flex items-center justify-center">
+        <div className=" flex-auto hidden sm:flex items-center justify-center">
           <div className="w-fit h-fit  flex flex-row bg-black/20 shadow-lg px-3 py-1 rounded-full items-center justify-center">
             <button className="w-fit h-fit px-2 py-1">About</button>
             <button className="w-fit h-fit px-2 py-1">Features</button>
@@ -76,11 +86,10 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <div className="relative w-fit h-fit  cursor-pointer">
+        <div className="hidden sm:block relative w-fit h-fit  cursor-pointer">
           {/* <button onClick={changeLanguage}>
             {language == "en" ? "မြန်မာ" : "English"}
           </button> */}
-
           <button
             className="w-fit h-fit text-sm flex items-center justify-center "
             onClick={() => setLanguageBox(!languageBox)}
@@ -114,9 +123,59 @@ const LandingPage = () => {
             </span>
           </div>
         </div>
+        <div className="w-fit sm:hidden h-fit flex flex-row gap-x-7 items-center justify-center">
+          <div className="">
+            <FaLanguage size={37} />
+          </div>
+          <div
+            onClick={() => setModalBox(!modalBox)}
+            className="block sm:hidden"
+          >
+            <CiMenuBurger size={25} />
+          </div>
+        </div>
       </nav>
-      <section className="w-full h-fit flex flex-col items-center justify-center">
-        <div className="w-[650px] h-fit bg-black/15 flex flex-col py-4 px-5 rounded-xl">
+      {modalBox ? (
+        <div className="absolute sm:hidden md:hidden xl:hidden flex flex-col left-0 bg-slate-600 w-2/3 h-full">
+          <div className="w-full h-full flex flex-col items-center pt-20 gap-y-9 ">
+            <button className="w-full h-fit px-2 py-1 flex flex-row items-center justify-between">
+              <span className="text-xl font-bold">About</span>
+              <IoIosArrowForward size={17} />
+            </button>
+            <button className="w-full h-fit px-2 py-1 flex flex-row items-center justify-between">
+              <span className="text-xl font-bold">Features</span>
+              <IoIosArrowForward size={17} />
+            </button>
+            <button className="w-full h-fit px-2 py-1 flex flex-row items-center justify-between">
+              <span className="text-xl font-bold">Tutorials</span>
+              <IoIosArrowForward size={17} />
+            </button>
+            <button className="w-full h-fit px-2 py-1 flex flex-row items-center justify-between">
+              <span className="text-xl font-bold">Support</span>
+              <IoIosArrowForward size={17} />
+            </button>
+          </div>
+          <div className="w-full h-[200px]  flex flex-col px-2 gap-y-4">
+            <button
+              onClick={() => navigate("/login")}
+              className="w-full h-fit rounded-md px-2 py-2 flex items-center justify-center gap-x-1 bg-blue-500 text-xl text-white font-extrabold"
+            >
+              SingUp <IoMdLogIn size={20} />
+            </button>
+            <button
+              onClick={() => navigate("/login")}
+              className="w-full h-fit rounded-md px-2 py-2 flex items-center justify-center gap-x-1 bg-white text-xl text-black font-extrabold"
+            >
+              Login <IoMdLogIn size={20} />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+
+      <section className="px-2 w-full h-fit flex flex-col items-center justify-center">
+        <div className="w-full sm:w-[650px] h-fit sm:h-[350px] sm:bg-black/15 flex flex-col sm:py-4 px-1  pt-3 sm:px-5 rounded-none sm:rounded-xl">
           <h1 className="font-bold text-lg">
             {welcome_content[language].title}
           </h1>
