9b29b2fcec5e133a5cd4ca61623c20183ef4f314
Zwe Nyi Nyi Htwe <zwenyinyihtwe@hotmail.com>
2025-03-08 11:28:08 +0630

    allowed origin updated

--- server/configs/allowedOrigins.js
+++ server/configs/allowedOrigins.js
@@ -1,12 +1,11 @@
 const allowedOrigins = [
-  "https://freedom-codes.onrender.com",
-  "https://freedom-codes.vercel.app",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:4173",
   "http://192.168.1.103:5173",
+  "https://www.netflow4mm.com",
 ];
 
 module.exports = allowedOrigins;
